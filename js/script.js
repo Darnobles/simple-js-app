39a37c7051f4d9c1f85524458828a82021162517
Darnobles <126102632+Darnobles@users.noreply.github.com>
2023-03-08 22:55:33 -0500

    feat: loop
    add loop that hightlight pokemon name and height

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 let pokemonList = [
-    {name: "charmander", height: 6, type: ["fire", "speed"]},
-    {name: "sandshrew", height: 6, type: ["ground", "rock"]},
-    {name: "kakuna", height: 6, type: ["bug", "poison"]}
+    {name: "charmander", height: 1.8, type: ["fire", "speed"]},
+    {name: "sandshrew", height: 1, type: ["ground", "rock"]},
+    {name: "kakuna", height: .5, type: ["bug", "poison"]}
 ];
+for (let i=0; i < pokemonList.height; i++) {
+    if (pokemonList [i].height < 2 && pokemonList[i].height > 1) {
+        document.write (pokemonList[i].name.height + "Wow, that's big! ");
+    }else {
+        document.write (pokemonList[i].name.height + "Average height");
+    }
+}
