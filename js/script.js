40a4eebcd3168599975b9ce7e047a485ac5cc441
Darnobles <126102632+Darnobles@users.noreply.github.com>
2023-03-11 00:10:51 -0500

    fix
    change syntax in line 6-8

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ let pokemonList = [
     {name: "sandshrew", height: 1, type: ["ground", "rock"]},
     {name: "kakuna", height: .5, type: ["bug", "poison"]}
 ];
-for (let i=0; i < pokemonList.height; i++) {
-    if (pokemonList [i].height < 2 && pokemonList[i].height > 1) {
-        document.write (pokemonList[i].name.height + "Wow, that's big! ");
+for (let i=0; i < pokemonList.length; i++) {
+    if (pokemonList [i].height > 1 && pokemonList[i].height < 2) {
+        document.write (pokemonList[i].name + " - " + pokemonList [i].height + "m - Wow, that's big! ");
     }else {
-        document.write (pokemonList[i].name.height + "Average height");
+        document.write (pokemonList[i].name + " - " + pokemonList [i].height + "m - Average height");
     }
 }
