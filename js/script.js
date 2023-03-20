85f11958c07688efd46d724a3953aa03fbb8dcbf
Darnobles <126102632+Darnobles@users.noreply.github.com>
2023-03-19 22:02:07 -0400

    refactor
    wrap pokemonList in IIFE

--- js/script.js
+++ js/script.js
@@ -6,4 +6,21 @@ let pokemonList = [
 
 pokemonList.forEach (function(pokemonList) {
     document.write (pokemonList.name + " - " + pokemonList.height + " - " + pokemonList.type);
-})
+});
+
+let pokemonRepository = (function () {
+    let pokemonList = [];
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    }
+})();
