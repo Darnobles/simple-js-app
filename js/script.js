b7f86e1ea1ae3f958533f19c1a35c4ee892b5290
Darnobles <126102632+Darnobles@users.noreply.github.com>
2023-04-01 22:28:13 -0400

    fix
    delete let pokemonList and forEash function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-let pokemonList = [
-    {name: "charmander", height: 1.8, type: ["fire", "speed"]},
-    {name: "sandshrew", height: 1, type: ["ground", "rock"]},
-    {name: "kakuna", height: .5, type: ["bug", "poison"]}
-];
-
-pokemonList.forEach (function(pokemon) {
-    let ul = document.querySelector('.pokemon-list');
-    let listItem = document.createElement('li');
-    let button = document.createElement('button');
-    button.innerText = (pokemon.name);
-    button.classList.add('pokebutton');
-    listItem.appendChild(button);
-    ul.appendChild(listItem);
-});
-
 let pokemonRepository = (function () {
     let pokemonList = [
     {name: "charmander", height: 1.8, type: ["fire", "speed"]},
