aadf5978b8e53fb641d1205366e255e97ea44287
Darnobles <126102632+Darnobles@users.noreply.github.com>
2023-04-03 23:40:50 -0400

    refactor
    add polyfills and api

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
-    {name: "charmander", height: 1.8, type: ["fire", "speed"]},
-    {name: "sandshrew", height: 1, type: ["ground", "rock"]},
-    {name: "kakuna", height: .5, type: ["bug", "poison"]}
-    ];
+    let pokemonList = []
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function add(pokemon) {
         pokemonList.push(pokemon);
@@ -39,4 +36,54 @@ let pokemonRepository = (function () {
         getAll: getAll,
         addListItem: addListItem
     };
+
+    function loadList() {
+        return fetch(apiUrl).then (function(response) {
+            return response.json();
+        }).then (function (json) {
+        json.results.foreach(function (item) {
+            let pokemon = {
+                name: item.name,
+                detailsUrl: item.url
+            };
+            add(pokemon);
+        });
+    }).catch(function (e) {
+        console.error(e);
+    })
+    }
+
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+        }).catch(function (e) {
+            console.error(e);
+        });
+
+    }
+
+    function showDetails(pokemon) {
+        loadDetails(pokemon).then(function () {
+            console.log(pokemon);
+        });
+    }
+
+    return {
+        add: add,
+        getAll: getAll,
+        loadList: loadList,
+        addListItem: addListItem,
+        loadDetails: loadDetails,
+        showDetails: showDetails
+    };
 })();
+
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
+});
